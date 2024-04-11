5777628d8d0d30d16c413586f74b62a254b5a30d
torikatsupg <torikatsu923@gmail.com>
2024-04-12 01:41:07 +0900

    feat: generate api client by orval

---
+++ orval.config.ts
@@ -0,0 +1,24 @@
+import { defineConfig } from "orval";
+
+export default defineConfig({
+  api: {
+    input: {
+      target: "./__api__/index.yaml",
+    },
+    output: {
+      target: "./src/gen",
+      mode: "tags-split",
+      client: "react-query",
+      mock: true,
+      override: {
+        mutator: {
+          path: "./src/httpClient.ts",
+          name: "httpClient",
+        },
+        query: {
+          useSuspenseQuery: true,
+        },
+      },
+    },
+  },
+});
